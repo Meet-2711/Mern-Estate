73bbec35ed19fbcfe287e2237c20c3b248824238
meet <118793930+Meet-2711@users.noreply.github.com>
2024-03-05 12:44:27 +0530

    complete update user functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Home from "./pages/Home"
 import SignUp from "./pages/SignUp"
 import About from "./pages/about"
-import SignIn from "./pages/signin"
+import SignIn from "./pages/SignIn"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
 import PrivateRoute from './components/PrivateRoute';
