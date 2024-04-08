37dcf399039ddbfe56fb91fae5a2f17ffcc7bb60
meet <118793930+Meet-2711@users.noreply.github.com>
2024-04-08 16:43:49 +0530

    fix about

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Home from "./pages/Home"
 import SignUp from "./pages/SignUp"
-import About from "./pages/about"
+import About from "./pages/About"
 import SignIn from "./pages/SignIn"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
